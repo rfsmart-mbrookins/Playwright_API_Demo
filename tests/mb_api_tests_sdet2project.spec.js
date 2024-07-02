a219c5b44a888a4818053627bf7673d6923d2003
Michael Brookins <michael.brookins@rfsmart.com>
2024-07-02 15:58:12 -0400

    practice

--- tests/mb_api_tests_sdet2project.spec.js
+++ tests/mb_api_tests_sdet2project.spec.js
@@ -1,35 +1,42 @@
 import { test, expect } from '@playwright/test';
-import { title } from 'process';
 
+// Test data
+const baseURL = "https://jsonplaceholder.typicode.com";
 
-// GET API Request
-test('API GET Request', async ({ request }) => {
-  const response = await request.get('https://jsonplaceholder.typicode.com/posts', {
-    ignoreHTTPSErrors: true,
+// Describe test cases
+test.describe.parallel("API Tests", () => {
+
+  // GET API Request
+  test('API GET Request', async ({ request }) => {
+    const response = await request.get(`${baseURL}/posts`, {
+      ignoreHTTPSErrors: true,
+    });
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    console.log(responseBody);
   });
-  expect(response.status()).toBe(200);
-  const responseBody = await response.json();
-  console.log(responseBody);
-});
 
-
-//POST API Request
-test('API Post Request', async ({ request }) => {
-  const response = await request.post('https://jsonplaceholder.typicode.com/posts', {
-    ignoreHTTPSErrors: true,
-    data: {
-      userId: 11,
-      id: 101,
-      title: "Jenny's Number",
-      body: "Hey Jenny, Jenny who can I turn to? You give me something I can hold on to. I know you'll think I'm like the others before. Who saw your name and number on the wall. Jenny I've got your number."
-    }
+  // POST API Request
+  test.only('API POST Request', async ({ request }) => {
+    const response = await request.post(`${baseURL}/posts`, {
+      ignoreHTTPSErrors: true,
+      data: {
+        userId: 11,
+        id: 101,
+        title: "Jenny's Number",
+        body: "Hey Jenny, Who saw your name and number on the wall. Jenny I've got your number. 867-5309."
+      }
+    });
+    expect(response.status()).toBe(201); // 201 is the correct status code for resource creation
+    const jsonResponse = await response.json();
+    // Verifying that the response contains the properties sent in the request
+    expect(jsonResponse).toHaveProperty('userId', 11);
+    expect(jsonResponse).toHaveProperty('id', 101);
+    expect(jsonResponse).toHaveProperty('title', "Jenny's Number");
+    console.log(jsonResponse);
   });
-  expect(response.status()).toBe(201); // 201 is the correct status code for resource creation
-  const jsonResponse = await response.json();
-  // Verifying that the response contains the properties sent in the request
-  expect(jsonResponse).toHaveProperty('userId', 11);
-  expect(jsonResponse).toHaveProperty('id', 101)
-  expect(jsonResponse).toHaveProperty('title', "Jenny's Number");
-  console.log(jsonResponse);
-});
 
+  /////
+  
+
+});
