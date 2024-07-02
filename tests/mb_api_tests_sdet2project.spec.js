0ad76fffe44797807680b21284d37068ee746e8b
Michael Brookins <michael.brookins@rfsmart.com>
2024-07-02 15:31:41 -0400

    updated api practice GET/POST

--- tests/mb_api_tests_sdet2project.spec.js
+++ tests/mb_api_tests_sdet2project.spec.js
@@ -1,44 +1,35 @@
 import { test, expect } from '@playwright/test';
+import { title } from 'process';
 
 
 // GET API Request
 test('API GET Request', async ({ request }) => {
-  const response = await request.get('https://petstore.swagger.io/v2/pet/findByStatus', {
-    data: {
-      status: 'available' // you can change the status as needed
-    },
-    ignoreHTTPSErrors: true
+  const response = await request.get('https://jsonplaceholder.typicode.com/posts', {
+    ignoreHTTPSErrors: true,
   });
   expect(response.status()).toBe(200);
   const responseBody = await response.json();
   console.log(responseBody);
 });
 
-// //POST API Request
-// test('API Post Request', async ({ request }) => {
-//     const response = await request.post('https://petstore.swagger.io/v2/pet', {
-//       ignoreHTTPSErrors: true,
-//       data: {
-//         "id": 8675309,
-//         "category": {
-//           "id": 8675309,
-//           "name": "Jenny"
-//         },
-//         "name": "doggie",
-//         "photoUrls": [
-//           "https://media.istockphoto.com/id/525497677/photo/party-dog.jpg?s=2048x2048&w=is&k=20&c=p7sYq_9bYQBDGKPC5XhAIrvy-AwXZExoeoqeheaID6U="
-//         ],
-//         "tags": [
-//           {
-//             "id": 8675309,
-//             "name": "Jenny"
-//           }
-//         ],
-//         "status": "available"
-//       }
-//     });
-//     expect(response.status()).toBe(200); // or 201 if you're creating a new resource
-//     const jsonResponse = await response.json();
-//     expect(jsonResponse).toHaveProperty('name', 'doggie'); // matching the data sent in the request
-//     console.log(jsonResponse);
-// });
+
+//POST API Request
+test('API Post Request', async ({ request }) => {
+  const response = await request.post('https://jsonplaceholder.typicode.com/posts', {
+    ignoreHTTPSErrors: true,
+    data: {
+      userId: 11,
+      id: 101,
+      title: "Jenny's Number",
+      body: "Hey Jenny, Jenny who can I turn to? You give me something I can hold on to. I know you'll think I'm like the others before. Who saw your name and number on the wall. Jenny I've got your number."
+    }
+  });
+  expect(response.status()).toBe(201); // 201 is the correct status code for resource creation
+  const jsonResponse = await response.json();
+  // Verifying that the response contains the properties sent in the request
+  expect(jsonResponse).toHaveProperty('userId', 11);
+  expect(jsonResponse).toHaveProperty('id', 101)
+  expect(jsonResponse).toHaveProperty('title', "Jenny's Number");
+  console.log(jsonResponse);
+});
+
